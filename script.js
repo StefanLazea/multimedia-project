74ebd283bfe905e660a7b2cdd686acdc8b320a27
Stefan <lazeastefan@gmail.com>
2019-12-08 01:23:09 +0200

    Add histogram

--- script.js
+++ script.js
@@ -8,10 +8,34 @@ function app() {
     let btn = document.querySelector('#btnHistogram');
     btn.addEventListener('click', drawHistogram);
 }
-function drawHistogram() {
-    console.log("apelat")
-}
 
+function drawHistogram() {
+    data = [2, 10, 8, 9, 1, 3, 6]
+
+    drawBasicCanvasBackground('white', 'black');
+
+    context.fillRect(0, 0, canvasW, canvasH);
+    context.strokeRect(0, 0, canvasW, canvasH);
+
+
+    context.fillStyle = 'red';
+    context.beginPath();
+
+    const number = data.length;
+    const w = canvasW / number;
+    const f = canvasH / Math.max(...data);
+
+    for (let i = 0; i < number; i++) {
+        let hi = data[i] * f;
+        context.rect(w * i, canvasH - hi, w, hi);
+    }
+    context.fill();
+    context.stroke();
+}
+function drawBasicCanvasBackground(fillColor, strokeStyle) {
+    context.fillStyle = fillColor;
+    context.strokeStyle = strokeStyle;
+}
 function initCanvas() {
     canvas = document.querySelector('canvas');
     context = canvas.getContext('2d');
