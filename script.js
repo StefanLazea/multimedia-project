354e35af67f2cce8eb1ca4e090c2d6140a497fcb
Stefan <lazeastefan@gmail.com>
2019-12-08 20:44:15 +0200

    Added datejson plus dropdown

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', app);
+const dataJson = { "options": { "firstCharact": "investments", "secondCharact": "inflation" }, "data": [{ "year": 2000, "countries": [{ "name": "Romania", "firstCharact": 19.8, "secondCharact": 43.1 }] }, { "year": 2001, "countries": [{ "name": "Romania", "firstCharact": 22.6, "secondCharact": 43.1 }] }] };
 let canvas, context;
 let canvasW, canvasH;
 //tema - grafic cu linii
@@ -10,7 +11,13 @@ function app() {
 }
 
 function drawHistogram() {
-    data = [2, 10, 8, 9, 1, 3, 6]
+    let getLateralDiv = document.getElementById('lateral-bar');
+    let chooseCharacteristic = document.createElement('select');
+    populateDropDown(chooseCharacteristic);
+    getLateralDiv.appendChild(chooseCharacteristic);
+
+    context.clearRect(0, 0, canvasW, canvasH);
+    data = [2, 10, 8, 9, 1, 3, 6, 10, 2, 4, 9, 12];
 
     drawBasicCanvasBackground('white', 'black');
 
@@ -18,7 +25,7 @@ function drawHistogram() {
     context.strokeRect(0, 0, canvasW, canvasH);
 
 
-    context.fillStyle = 'red';
+    context.fillStyle = 'orange';
     context.beginPath();
 
     const number = data.length;
@@ -30,12 +37,28 @@ function drawHistogram() {
         context.rect(w * i, canvasH - hi, w, hi);
     }
     context.fill();
+    context.lineWidth = 1;
     context.stroke();
 }
+
+function populateDropDown(dropdown) {
+
+    for (let option in dataJson.options) {
+        var op = new Option();
+        op.value = 1
+        op.text = dataJson.options[option];
+        dropdown.add(op)
+    }
+    dropdown.style.display = "block"
+    dropdown.style.margin = "10 auto"
+}
+
 function drawBasicCanvasBackground(fillColor, strokeStyle) {
     context.fillStyle = fillColor;
     context.strokeStyle = strokeStyle;
+    context.lineWidth = 0;
 }
+
 function initCanvas() {
     canvas = document.querySelector('canvas');
     context = canvas.getContext('2d');
